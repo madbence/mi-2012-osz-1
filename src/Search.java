414ff5f85f46787e96fb8543a62e855e5fb0f88d
Bence Dányi <madbence@gmail.com>
2012-09-20 21:13:59 +0200

    Main class implemented

--- src/Search.java
+++ src/Search.java
@@ -1,3 +1,6 @@
+
+import java.io.File;
+
 /**
  *
  * @author lennon
@@ -8,6 +11,15 @@ public class Search {
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		// TODO code application logic here
+		try {
+			FileParser parser = new FileParser(new File("examples/" + args[0]));
+			parser.parse();
+			DebugFrame debugFrame = new DebugFrame();
+			debugFrame.setGraph(parser.getGraph());
+			debugFrame.setStartNode(parser.getStartNode());
+			debugFrame.setEndNodes(parser.getEndNodes());
+			debugFrame.setVisible(true);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
-}
