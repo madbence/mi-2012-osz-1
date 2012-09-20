40771484e395b91ba66050ab01b877739d387b4b
Bence Dányi <madbence@gmail.com>
2012-09-20 19:26:28 +0200

    Getters for Graph boundaries

--- src/Graph.java
+++ src/Graph.java
@@ -31,4 +31,48 @@ public class Graph {
 	public int getEdgeCount() {
 		return edges.size();
 	}
+
+	public int getMinX() {
+		if(nodes.isEmpty()) {
+			return 0;
+		}
+		int min = nodes.get(0).getX();
+		for(Node n : nodes) {
+			min = n.getX() < min ? n.getX() : min;
+		}
+		return min;
+	}
+
+	public int getMinY() {
+		if(nodes.isEmpty()) {
+			return 0;
+		}
+		int min = nodes.get(0).getY();
+		for(Node n : nodes) {
+			min = n.getY() < min ? n.getY() : min;
+		}
+		return min;
+	}
+
+	public int getMaxX() {
+		if(nodes.isEmpty()) {
+			return 0;
+		}
+		int max = nodes.get(0).getX();
+		for(Node n : nodes) {
+			max = n.getX() > max ? n.getX() : max;
+		}
+		return max;
+	}
+
+	public int getMaxY() {
+		if(nodes.isEmpty()) {
+			return 0;
+		}
+		int max = nodes.get(0).getY();
+		for(Node n : nodes) {
+			max = n.getY() > max ? n.getY() : max;
+		}
+		return max;
+	}
 }
